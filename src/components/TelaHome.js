f14a6c666b6bf0a31948d3e205a692ec928fbfdb
Stephanie Flavio <stephdpmello@gmail.com>
2021-09-27 17:01:18 -0300

    Ajustei os botoes ninjas da tela home

--- src/components/TelaHome.js
+++ src/components/TelaHome.js
@@ -2,14 +2,21 @@ import React from "react";
 import styled from 'styled-components'
 // import Axios from 'axios'
 
+
 const MainApp = styled.main`
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	flex-direction: column;
 	margin-top: 100px;
+    
 `
 
+const ButtonHome = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 export default class TelaCadastro extends React.Component {
     render() {
@@ -18,9 +25,11 @@ export default class TelaCadastro extends React.Component {
                 <MainApp>
 					<h1>LabeNinjas</h1>
 					<p>O talento certo no momento certo</p>
+                <ButtonHome>
 					<button onClick={this.props.irParaCadastro}>Quero ser um Ninja</button>
 					<button onClick={this.props.irParaServicos}>Contratar um Ninja</button>
-				</MainApp>
+				</ButtonHome>
+                </MainApp>
             </div>
         )
     }
